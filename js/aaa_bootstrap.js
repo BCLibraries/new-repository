9d5d98178ba90781c66ca954b9324f3e5e87a69e
Luke Gaudreau <gaudreal@bc.edu>
2018-12-21 11:43:30 -0500

    Enable browzine integration

--- js/aaa_bootstrap.js
+++ js/aaa_bootstrap.js
@@ -6,5 +6,13 @@ var app = angular.module('viewCustom', [
     'bc-permalink',
     'bc-search-collections',
     'bc-gallery-item',
-    'bc-toc-link',
+    'bc-browzine',
 ]);
+window.browzine = {
+    api: "https://public-api.thirdiron.com/public/v1/libraries/XXX",
+    apiKey: "XXXX",
+};
+
+browzine.script = document.createElement("script");
+browzine.script.src = "https://s3.amazonaws.com/browzine-adapters/primo/browzine-primo-adapter.js";
+document.head.appendChild(browzine.script);
